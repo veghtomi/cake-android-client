9faf2434c72154b035d7bc86a55ffd54be49c1d7
veghtomi <tommi.vegh@gmail.com>
2018-04-04 13:03:28 +0200

    Add refresh function

--- app/src/main/java/com/waracle/androidtest/screens/cakes/CakesFragment.java
+++ app/src/main/java/com/waracle/androidtest/screens/cakes/CakesFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.waracle.androidtest.R;
 import com.waracle.androidtest.model.Cake;
 import com.waracle.androidtest.screens.cakes.adapter.CakeAdapter;
 
@@ -25,10 +27,26 @@ public class CakesFragment extends ListFragment implements CakesContract.View {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        setHasOptionsMenu(true);
+
         presenter = new CakesPresenter();
 
         adapter = new CakeAdapter();
         setListAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_refresh:
+                presenter.loadCakes();
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
